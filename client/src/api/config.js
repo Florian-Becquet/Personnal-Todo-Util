34903e10794d0d68ae9f6988bc24f4e0f4f6630f
florian becquet <florian.becquet1@gmail.com>
2024-04-01 13:24:36 +0200

    styles material ui

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,4 +1,4 @@
-// export const API_URL = `https://personnal-todo-util.onrender.com`;
-export const API_URL = `http://localhost:8080`;
+export const API_URL = `https://personnal-todo-util.onrender.com`;
+// export const API_URL = `http://localhost:8080`;
 // export const API_URL_NOTES = `http://localhost:8081`;
 // export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcwODQzNjA3Mn0.XXPajjlTQqKPDFvyNUUL6z6AlYQ9Mg35gDZYMaXT5zA';
