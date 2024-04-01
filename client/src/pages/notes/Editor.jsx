4ecd6a96fafe630d0933e14b3c575460d1680a3f
florian becquet <florian.becquet1@gmail.com>
2024-04-01 14:47:50 +0200

    delete Autofocus

--- client/src/pages/notes/Editor.jsx
+++ client/src/pages/notes/Editor.jsx
@@ -13,7 +13,7 @@ export const Editor = ({ text, setText }) => {
     return (
         <div className='editor'>
             <div>Markdown</div>
-            <textarea  onKeyDown={(e) => { if (e.keyCode === 9) e.preventDefault() }} spellCheck="false" required autoFocus value={text} onChange={(e) => setText(e.target.value)}>{text}</textarea>
+            <textarea  onKeyDown={(e) => { if (e.keyCode === 9) e.preventDefault() }} spellCheck="false" required value={text} onChange={(e) => setText(e.target.value)}>{text}</textarea>
         </div>
     )
 }
